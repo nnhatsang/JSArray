33ced7496d89c6c02d20d333f68d0965d6be1661
nnhatsang <nhatsang57@gmail.com>
2023-10-26 01:45:22 +0700

    update

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 function getEl(n) {
   return document.getElementById(n);
 }
-var exportArr = [];
+var exportArr = [],
+  arrEx8 = [];
 function addNum() {
   var add = Number(getEl("inputArr").value);
   exportArr.push(add);
@@ -82,9 +83,56 @@ function findFirstPrimeInArray(arr) {
       return number;
     }
   }
-  return null; 
+  return null;
 }
 function Ex7() {
   const result = findFirstPrimeInArray(exportArr);
   getEl("Ex_7").innerHTML = result;
+}
+function addNumFloat() {
+  var add = Number(getEl("inputArr8").value);
+  arrEx8.push(add);
+  getEl("outArr8").innerHTML = arrEx8;
+  getEl("inputArr8").value = "";
+}
+function Ex8() {
+  for (var dem = 0, i = 0; i < arrEx8.length; i++)
+    Number.isInteger(arrEx8[i]) && dem++;
+  getEl("Ex_8").innerHTML = dem;
+}
+
+function swap(arr, index1, index2) {
+  [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+}
+
+function Ex9() {
+  const index1 = parseInt(getEl("inputIndex1").value, 10);
+  const index2 = parseInt(getEl("inputIndex2").value, 10);
+
+  if (
+    !isNaN(index1) &&
+    !isNaN(index2) &&
+    index1 >= 0 &&
+    index2 >= 0 &&
+    index1 < exportArr.length &&
+    index2 < exportArr.length
+  ) {
+    swap(exportArr, index1, index2);
+    getEl("Ex_9").innerHTML = "Mảng sau khi đổi: " + exportArr;
+  } else {
+    getEl("Ex_9").innerHTML = "Vị trí không hợp lệ.";
+  }
+}
+function Ex10() {
+  for (var demDuong = 0, demAm = 0, i = 0; i < exportArr.length; i++)
+    exportArr[i] > 0 ? demDuong++ : exportArr[i] < 0 && demAm++;
+  var result =
+    demDuong > demAm
+      ? "Số dương > Số âm"
+      : demDuong < demAm
+      ? "Số âm > Số dương"
+      : "Số dương = Số âm";
+  getEl(
+    "Ex_10"
+  ).innerHTML = `Có ${demAm} số âm và ${demDuong} số dương trong mảng. Như vậy, ${result}`;
 }
